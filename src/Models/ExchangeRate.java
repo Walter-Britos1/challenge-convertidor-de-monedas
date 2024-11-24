ed0fd1112ca51c931e80dd2f884cd3ee4d74ebd2
Walter-Britos1 <walterbritos418@gmail.com>
2024-11-23 23:37:55 -0300

    create class ExchangeRate

--- src/Models/ExchangeRate.java
+++ src/Models/ExchangeRate.java
@@ -1,5 +1,34 @@
 package Models;
 
-public class ExchangeRate {
+import java.util.Map;
 
+public class ExchangeRate {
+    private String result; // Resultado de la consulta (success)
+    private String base_code; // Moneda base
+    private Map<String, Double> conversion_rates; // Tasas de conversión
+
+    // Getters y Setters
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public String getBase_code() {
+        return base_code;
+    }
+
+    public void setBase_code(String base_code) {
+        this.base_code = base_code;
+    }
+
+    public Map<String, Double> getConversion_rates() {
+        return conversion_rates;
+    }
+
+    public void setConversion_rates(Map<String, Double> conversion_rates) {
+        this.conversion_rates = conversion_rates;
+    }
 }
